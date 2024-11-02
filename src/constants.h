ceeb05ff1a9e3a1249dedd9a7edfd06d1119faf7
Avjot Singh <avjot.singh99@gmail.com>
2024-11-02 17:02:30 -0400

    add key placeholders for signatures

--- src/constants.h
+++ src/constants.h
@@ -5,4 +5,5 @@
 namespace Constants {
     extern std::vector<std::string> serverNames;
     extern std::map<std::string, std::string> serverAddresses;
+    extern std::map<std::string, std::map<std::string, std::string>> keys;
 }
